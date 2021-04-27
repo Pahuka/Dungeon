e6862050f99d1aea3bf922f975c8ac17864e85d1
Pahuka <J-a-c-k@yandex.ru>
2021-04-27 14:53:54 +0500

    Доделал метод FindPaths, все тесты проходят. Осталась оптимизация.

--- BfsTask.cs
+++ BfsTask.cs
@@ -1,14 +1,24 @@
-using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dungeon
 {
     public class BfsTask
     {
+        public static void PointGeneration(HashSet<Point> visit, Map map, Queue<Point> qVisit, Point point)
+        {
+            for (var dy = -1; dy <= 1; dy++)
+                for (var dx = -1; dx <= 1; dx++)
+                    if (dx != 0 && dy != 0) continue;
+                    else
+                    {
+                        var tPoint = new Point { X = point.X + dx, Y = point.Y + dy };
+                        if (!visit.Contains(tPoint) & map.InBounds(tPoint) & !qVisit.Contains(tPoint))
+                            qVisit.Enqueue(new Point { X = point.X + dx, Y = point.Y + dy });
+                    }
+        }
+
         public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Point[] chests)
         {
             var chestMap = new HashSet<Point>();
@@ -18,9 +28,7 @@ namespace Dungeon
             var result = new HashSet<SinglyLinkedList<Point>>();
 
             foreach (var item in chests)
-            {
                 chestMap.Add(item);
-            }
 
             queue.Enqueue(start);
 
@@ -28,43 +36,37 @@ namespace Dungeon
             {
                 point = queue.Dequeue();
 
-                if (!map.InBounds(point) || map.Dungeon[point.X, point.Y] != MapCell.Empty) continue;
+                if (map.Dungeon[point.X, point.Y] != MapCell.Empty) continue;
 
                 if (!visitMap.Contains(point))
                 {
                     visitMap.Add(point);
+
                     if (result.Count == 0) result.Add(new SinglyLinkedList<Point>(point));
                     else
                     {
-                        foreach (var item in visitMap)
-                        {
-                            if (item == new Point(point.X - 1, point.Y))
-                                result.Add(new SinglyLinkedList<Point>(point, result
-                                    .SingleOrDefault(x => x.Value == new Point(point.X - 1, point.Y))));
-                            if (item == new Point(point.X, point.Y - 1))
-                                result.Add(new SinglyLinkedList<Point>(point, result
-                                    .SingleOrDefault(x => x.Value == new Point(point.X, point.Y - 1))));
-                            if (item == new Point(point.X + 1, point.Y))
-                                result.Add(new SinglyLinkedList<Point>(point, result
-                                    .SingleOrDefault(x => x.Value == new Point(point.X + 1, point.Y))));
-                            if (item == new Point(point.X, point.Y + 1))
-                                result.Add(new SinglyLinkedList<Point>(point, result
-                                    .SingleOrDefault(x => x.Value == new Point(point.X, point.Y + 1))));
-                        }
+                        if (visitMap.Contains(new Point(point.X - 1, point.Y)))
+                            result.Add(new SinglyLinkedList<Point>(point, result
+                                .First(x => x.Value == new Point(point.X - 1, point.Y))));
+                        if (visitMap.Contains(new Point(point.X, point.Y - 1)))
+                            result.Add(new SinglyLinkedList<Point>(point, result
+                                .First(x => x.Value == new Point(point.X, point.Y - 1))));
+                        if (visitMap.Contains(new Point(point.X + 1, point.Y)))
+                            result.Add(new SinglyLinkedList<Point>(point, result
+                                .First(x => x.Value == new Point(point.X + 1, point.Y))));
+                        if (visitMap.Contains(new Point(point.X, point.Y + 1)))
+                            result.Add(new SinglyLinkedList<Point>(point, result
+                                .First(x => x.Value == new Point(point.X, point.Y + 1))));
 
-                        if (chestMap.Contains(point)) yield return result.Last();
+                        if (chestMap.Contains(point))
+                        {
+                            chestMap.Remove(point);
+                            yield return result.Last();
+                        }
                     }
                 }
 
-                for (var dy = -1; dy <= 1; dy++)
-                    for (var dx = -1; dx <= 1; dx++)
-                        if (dx != 0 && dy != 0) continue;
-                        else
-                        {
-                            var tPoint = new Point { X = point.X + dx, Y = point.Y + dy };
-                            if (!visitMap.Contains(tPoint))
-                                queue.Enqueue(new Point { X = point.X + dx, Y = point.Y + dy });
-                        }
+                if (chestMap.Count != 0) PointGeneration(visitMap, map, queue, point);
             }
 
             yield break;
