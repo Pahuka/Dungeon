85cfb36bee3aac979423dc0844717de48bb20421
Pahuka <J-a-c-k@yandex.ru>
2021-04-28 16:08:39 +0500

    Сделал рефакторинг метода FindPaths

--- BfsTask.cs
+++ BfsTask.cs
@@ -5,6 +5,24 @@ namespace Dungeon
 {
     public class BfsTask
     {
+        public static void PointGeneration(HashSet<Point> visit, Map map, 
+            Queue<Point> qVisit, Dictionary<Point, SinglyLinkedList<Point>> result, Point point)
+        {
+            for (var dy = -1; dy <= 1; dy++)
+                for (var dx = -1; dx <= 1; dx++)
+                    if (dx != 0 && dy != 0) continue;
+                    else
+                    {
+                        var tPoint = new Point { X = point.X + dx, Y = point.Y + dy };
+                        if (!visit.Contains(tPoint) & map.InBounds(tPoint) & !qVisit.Contains(tPoint))
+                        {
+                            qVisit.Enqueue(tPoint);
+                            visit.Add(tPoint);
+                            result.Add(tPoint, new SinglyLinkedList<Point>(tPoint, result[point]));
+                        }
+                    }
+        }
+
         public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Point[] chests)
         {
             var visitMap = new HashSet<Point>();
@@ -21,19 +39,7 @@ namespace Dungeon
 
                 if (map.Dungeon[point.X, point.Y] != MapCell.Empty) continue;
 
-                for (var dy = -1; dy <= 1; dy++)
-                    for (var dx = -1; dx <= 1; dx++)
-                        if (dx != 0 && dy != 0) continue;
-                        else
-                        {
-                            var tPoint = new Point { X = point.X + dx, Y = point.Y + dy };
-                            if (!visitMap.Contains(tPoint) & map.InBounds(tPoint) & !queue.Contains(tPoint))
-                            {
-                                queue.Enqueue(tPoint);
-                                visitMap.Add(tPoint);
-                                result.Add(tPoint, new SinglyLinkedList<Point>(tPoint, result[point]));
-                            }
-                        }
+                PointGeneration(visitMap, map, queue, result, point);
             }
 
             foreach (var item in chests)
