3a1382f81dd859fdc507657f527b531a01508ea6
Pahuka <J-a-c-k@yandex.ru>
2021-04-29 16:40:16 +0500

    Начал реализацию метода FindShortestPath

--- DungeonTask.cs
+++ DungeonTask.cs
@@ -6,13 +6,73 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Dungeon
 {
 	public class DungeonTask
 	{
-		public static MoveDirection[] FindShortestPath(Map map)
+        public static List<Tuple<Point, Point>> PathBigrams(List<Point> items)
+        {
+            var result = new List<Tuple<Point, Point>>();
+
+            if (items.Count == 0) return null;
+            else
+            {
+                var tempValue = items[0];
+                int index = 0;
+                foreach (var point in items)
+                {
+                    index++;
+                    if (index == 2)
+                    {
+                        result.Add(Tuple.Create(tempValue, point));
+                        index = 1;
+                        tempValue = point;
+                    }
+                    tempValue = point;
+                }
+            }
+
+            return result;
+        }
+
+        public static MoveDirection[] FindShortestPath(Map map)
 		{
-			return new MoveDirection[0];
+            var startPath = BfsTask.FindPaths(map, map.InitialPosition, map.Chests);
+            var endPath = BfsTask.FindPaths(map, map.Exit, map.Chests);
+            var pathToExit = BfsTask.FindPaths(map, map.InitialPosition, new Point[] { map.Exit });
+            var pointList = new List<Point>();
+
+            if (pathToExit.Count() == 0) return new MoveDirection[0];
+            if (startPath.Count() != 0 && endPath.Count() != 0)
+            {
+                pointList.AddRange(endPath
+                    .OrderBy(x => x.Length)
+                    .First().Reverse());
+                if (startPath.Where(x => x.Value == pointList.Last()).Count() == 0) return new MoveDirection[0];
+                pointList.AddRange(startPath
+                    .Where(x => pointList.Contains(x.Value))
+                    .SelectMany(x => x));
+                pointList = pointList.Distinct().Reverse().ToList();
+            }
+            else
+            {
+                pointList.AddRange(pathToExit.SelectMany(x => x).Reverse());
+            }
+
+            var bigramList = PathBigrams(pointList);
+            var resultPath = new MoveDirection[bigramList.Count];
+
+            for (int i = 0; i < bigramList.Count; i++)
+            {
+                if (bigramList[i].Item1.X == bigramList[i].Item2.X)
+                    resultPath[i] = bigramList[i].Item1.Y < bigramList[i].Item2.Y 
+                        ? MoveDirection.Down : MoveDirection.Up;
+                else resultPath[i] = bigramList[i].Item1.X < bigramList[i].Item2.X
+                        ? MoveDirection.Right : MoveDirection.Left;
+            }
+
+            return resultPath;
 		}
 	}
 }
