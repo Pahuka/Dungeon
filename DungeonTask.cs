f2cc76432f5cca28aaf8fa7f9ba4fefdae18e6f3
Pahuka <J-a-c-k@yandex.ru>
2021-05-05 11:32:31 +0500

    Переделал метод FindShortestPath, проблема с оптимизацией не решена.

--- DungeonTask.cs
+++ DungeonTask.cs
@@ -3,73 +3,72 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 
-
 namespace Dungeon
 {
-	public class DungeonTask
-	{
-        public static List<Tuple<Point, Point>> PathBigrams(List<Point> items)
-        {
-            var result = new List<Tuple<Point, Point>>();
-
-            if (items.Count == 0) return null;
-            else
-            {
-                var tempValue = items[0];
-                int index = 0;
-                foreach (var point in items)
-                {
-                    index++;
-                    if (index >= 2 & tempValue != point)
-                    {
-                        result.Add(Tuple.Create(tempValue, point));
-                        index = 1;
-                        //tempValue = point;
-                    }
-                    tempValue = point;
-                }
-            }
-
-            return result;
-        }
-
+    public class DungeonTask
+    {
         public static MoveDirection[] FindShortestPath(Map map)
-		{
+        {
             var startPath = BfsTask.FindPaths(map, map.InitialPosition, map.Chests);
-            var endPath = BfsTask.FindPaths(map, map.Exit, map.Chests);
+            var endPath = startPath
+                .Select(chest => Tuple.Create(chest, BfsTask.FindPaths(map, chest.Value, new[] { map.Exit })
+                .FirstOrDefault())).MinElement();
             var pathToExit = BfsTask.FindPaths(map, map.InitialPosition, new Point[] { map.Exit }).FirstOrDefault();
-            var pointList = new List<Point>();
 
             if (pathToExit == null) return new MoveDirection[0];
-            if (startPath.Count() != 0 && endPath.Count() != 0)
-            {
-                pointList.AddRange(endPath
-                    .OrderBy(x => x.Length)
-                    .First().Reverse());
-                if (startPath.Where(x => x.Value == pointList.Last()).Count() == 0) return new MoveDirection[0];
-                pointList.AddRange(startPath
-                    .Where(x => pointList.Contains(x.Value))
-                    .SelectMany(x => x));
-                pointList.Reverse();
-            }
-            else
-            {
-                pointList.AddRange(pathToExit.Reverse().ToList());
-            }
+            if (endPath == null) return pathToExit.Reverse().ToList().ParseDirection().ToArray();
 
-            var bigramList = PathBigrams(pointList);
-            var resultPath = new MoveDirection[bigramList.Count];
+            return endPath.Item1.Reverse().ToList().ParseDirection()
+                .Concat(endPath.Item2.Reverse().ToList().ParseDirection()).ToArray();
+        }
+    }
 
-            for (int i = 0; i < bigramList.Count; i++)
+    public static class ExtentionMetods
+    {
+        public static Tuple<SinglyLinkedList<Point>, SinglyLinkedList<Point>>
+            MinElement(this IEnumerable<Tuple<SinglyLinkedList<Point>, SinglyLinkedList<Point>>> items)
+        {
+            if (items.Count() == 0 || items.First().Item2 == null) return null;
+
+            var minValue = int.MaxValue;
+            var minElement = items.First();
+
+            foreach (var item in items)
             {
-                if (bigramList[i].Item1.X == bigramList[i].Item2.X)
-                    resultPath[i] = bigramList[i].Item1.Y < bigramList[i].Item2.Y 
-                        ? MoveDirection.Down : MoveDirection.Up;
-                else resultPath[i] = bigramList[i].Item1.X < bigramList[i].Item2.X
-                        ? MoveDirection.Right : MoveDirection.Left;
+                if ((item.Item1.Length + item.Item2.Length) < minValue)
+                {
+                    minValue = item.Item1.Length + item.Item2.Length;
+                    minElement = item;
+                }
             }
+            return minElement;
+        }
 
-            return resultPath;
-		}
-	}
+        public static MoveDirection GetDirection(Point one, Point two)
+        {
+            if (one.X == two.X)
+                return one.Y < two.Y ? MoveDirection.Down : MoveDirection.Up;
+            else return one.X < two.X ? MoveDirection.Right : MoveDirection.Left;
+        }
+
+        public static MoveDirection[] ParseDirection(this List<Point> items)
+        {
+            var result = new List<MoveDirection>();
+            if (items == null) return new MoveDirection[0];
+            var index = 0;
+            var tempValue = items.First();
+
+            foreach (var point in items)
+            {
+                index++;
+                if (index >= 2 & tempValue != point)
+                {
+                    result.Add(GetDirection(tempValue, point));
+                    index = 1;
+                }
+                tempValue = point;
+            }
+            return result.ToArray();
+        }
+    }
 }
